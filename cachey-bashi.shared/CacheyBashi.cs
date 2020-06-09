ecb977eb2e2e452f8ea114ae89a1ea1a48e93846
John Haines <john@Johns-Macbook-Pro.local>
2020-06-09 23:35:54 +0100

    WIP index file / index mem

--- cachey-bashi.shared/CacheyBashi.cs
+++ cachey-bashi.shared/CacheyBashi.cs
@@ -1,16 +1,72 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace cachey_bashi
 {
-    public class CacheyBashi
+    public class CacheyBashi<T>
     {
-        /// <summary>
-        /// Create a new instance of CacheyBashi
-        /// </summary>
-        /// <param name="cbFile">Path to the index.cb file (will be created if it doesn't exits)</param>
-        public CacheyBashi(string cbFile)
+        private string _dir;
+        private string _dbName;
+
+        private string _indexFile;
+        private string _keyFile;
+        private string _datFile;
+
+        private CbIndex<T> _cbIndex;
+        
+        public CacheyBashi(string directory, string dbName)
         {
             //todo: load the index.cb files into memory
+            _dir = directory;
+            _dbName = dbName;
+            _indexFile = Path.Combine(_dir, dbName) + ".index";
+            _keyFile = Path.Combine(_dir, dbName) + ".key";
+            _datFile = Path.Combine(_dir, dbName) + ".dat";
+            _cbIndex = new CbIndex<T>(_indexFile);
         }
+
+        public byte[] GetValue(byte[] key)
+        {
+            //read in-mem index to get addr of first 4bytes of key in key file (starting point for keys beginning with e.g. 0x1234)
+            //possibly want the last index of the e.g 0x1234 so we don't need to continually compare the first 4 bytes.
+            
+            //start reading from the key file a ulong at a time, use bitwise to compare  
+        }
+        
+        // public void Insert(byte[] key, byte[] data)
+        // {
+        //     //todo: not sure how to do this one, will need some sort of buffer of newly added values followed by a 'compaction' of sorts
+        //     //not really the point of this project's intended purpose but might be useful in future.
+        // }
+
+        public void Write(IEnumerable<KeyValuePair<byte[], byte[]>> data)
+        {
+            var indexFile = $"{_indexFile}.tmp";
+            var datFile = $"{_indexFile}.tmp";
+            var keyFile = $"{_indexFile}.tmp";
+            //todo: Write the index, key, and data files.  
+            
+            //take batches of 100k? arbitraty or maybe roughly calc mem requirements
+            var keyDataArray = new KeyData[100000];
+            var index = 0;
+            var datFileIndex = 0;
+
+            foreach (var kvp in data)
+            {
+                keyDataArray[index++].Key=kvp.Key;
+                keyDataArray[index].DataFileAddress = datFileIndex;
+                keyDataArray[index].DataLength = kvp.Value.Length;
+                //need to write the dat file here so we can discard data from memory
+                datFileIndex += kvp.Value.Length;
+            }
+        }
+    }
+
+    struct KeyData
+    {
+        public byte[] Key;
+        public long DataFileAddress;
+        public long DataLength;
     }
 }
---
+++ cachey-bashi.shared/CbIndex.cs
@@ -0,0 +1,136 @@
+using System;
+using System.IO;
+
+namespace cachey_bashi
+{
+    public class CbIndex
+    {
+        //data format
+        //0x00: 1 byte indicating the index key size
+        //0x01..n: 8 bytes indicating memory address of the beginning of the keys in the key file for this address offset
+        //e.g. 0x9998(+1) contains the start address for keys beginning with 0x9998 in the key file 
+        private byte[] _indexData;
+        
+        private string _file;
+        private byte _indexKeyLen;
+
+        private int _requiredDatLength;
+
+        private static readonly int _ulongSize = sizeof(ulong);
+
+        private readonly int _keyShift;
+        
+        public CbIndex(string indexFile, byte indexKeyLen = 2, bool overwriteNew = false)
+        {
+            if (indexKeyLen > sizeof(ulong))
+                throw new ArgumentException($"Index key length cannot be larger than {sizeof(ulong)}");
+            
+            _file = indexFile;
+            _indexKeyLen = indexKeyLen;
+            _requiredDatLength = CalculateRequiredLength(_indexKeyLen);
+            _indexData = new byte[_requiredDatLength];
+            _keyShift = _ulongSize - (_indexKeyLen * 2);
+            if (overwriteNew)
+            {
+                File.Delete(indexFile);
+            }
+            else
+            {
+                LoadFromDisk();    
+            }
+        }
+
+        int CalculateRequiredLength(byte indexKeyLen)
+        {
+            //max number representable by index key byte size
+            var maxAddrCount = (long)Math.Pow(2, _indexKeyLen * sizeof(byte));
+            return (int)(maxAddrCount * sizeof(ulong)) + 1;
+        }
+        
+        public void WriteToDisk()
+        {
+            using var file = File.Open(_file, FileMode.CreateNew);
+            using var writter = new BinaryWriter(file);
+            writter.Write(BitConverter.GetBytes(_indexKeyLen));
+            writter.Write(_indexData);
+        }
+
+        public void LoadFromDisk()
+        {
+            if (!File.Exists(_file))
+                return;
+            
+            //todo: read the file
+            using var file = File.Open(_file, FileMode.Open);
+            using var reader = new BinaryReader(file);
+            var indexKeyLen = reader.ReadByte();
+            if (indexKeyLen != _indexKeyLen)
+            {
+                throw new ArgumentException($"Index key length error: expected {_indexKeyLen}, however index file has {indexKeyLen}");
+            }
+
+            if (file.Length != _requiredDatLength)
+            {
+                throw new ArgumentException($"Index file length is incorrect, expected {_requiredDatLength}, got {file.Length}");
+            }
+
+            reader.Read(_indexData, 1, _requiredDatLength - 1);
+        }
+
+        public unsafe void SetStartIndexForKey(byte[] key, ulong addrStart)
+        {
+            var index = GetKeyIndexFromKey(key);
+            fixed (byte* pIndex = &_indexData[index])
+            {
+                (*(ulong*) pIndex) = addrStart;
+            }
+        }
+
+        unsafe int GetKeyIndexFromKey(byte[] key)
+        {
+            if (key.Length < _indexKeyLen)
+            {
+                throw new ArgumentException("key must be larger than the key index");
+            }
+            
+            int indexLocation = -1; 
+            if (key.Length >= _ulongSize)
+            {
+                fixed (byte* pKey = &key[0])
+                {
+                    indexLocation = (int)(*(ulong*) pKey >> _keyShift) + 1;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _indexKeyLen; i++)
+                {
+                    indexLocation += key[i] << ((_indexKeyLen - i)<<3);
+                }
+            }
+
+            return indexLocation;
+        }
+        
+        public unsafe ulong GetStartAddressForKey(byte[] key)
+        {
+            var indexLocation = GetKeyIndexFromKey(key);
+            if (indexLocation > 0)
+            {
+                fixed (byte* pIndex = &_indexData[indexLocation])
+                {
+                    return *(ulong*)pIndex;
+                }
+            }
+            return 0;
+        }
+        
+        // private void TypeCheck()
+        // {
+        //     if (!_validTypes.Contains(typeof(T)))
+        //     {
+        //         throw new ArgumentException("Type of T must one of: " + string.Concat(_validTypes, ", "));
+        //     }
+        // }
+    }
+}
